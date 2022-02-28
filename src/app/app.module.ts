a803ab8fea06ba0c43330061d30ab3a3bf729f0f
“Mayara <mayara.pimentels@gmail.com>
2022-02-28 20:03:35 -0300

    add search user by name challenge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PalindromeComponent } from './components/palindrome/palindrome.component';
+import { PalindromeComponent } from './challenges/palindrome/palindrome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchUserByNameComponent } from './challenges/search-user-by-name/search-user-by-name.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PalindromeComponent
+    PalindromeComponent,
+    SearchUserByNameComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/challenges/search-user-by-name/search-user-by-name.component.ts
@@ -0,0 +1,44 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+
+export interface User {
+  name: string;
+  phone: string;
+}
+
+@Component({
+  selector: 'app-search-user-by-name',
+  templateUrl: './search-user-by-name.component.html',
+  styleUrls: ['./search-user-by-name.component.scss']
+})
+export class SearchUserByNameComponent implements OnInit, AfterViewInit {
+  listUsers: User[];
+  displayedColumns: string[] = ['name', 'phone'];
+  dataSource = new MatTableDataSource<User>([]);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private http: HttpClient) { }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngOnInit() {
+    this._getListUsers();
+  }
+
+  private _getListUsers(): void {
+    this.http.get('assets/sample.json').subscribe((data: any)=> {
+      this.listUsers = data;
+      this.dataSource.data = data;
+    });
+  }
+
+  searchByName(name: string): void {
+    this.dataSource.data = this.listUsers.filter(data => data.name.toLowerCase().includes(name.toLowerCase()));
+  }
+
+}
